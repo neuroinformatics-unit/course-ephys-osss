a2a52de5db604a47fd44d1680bf2f1546e20cad9
JoeZiminski <joseph.j.ziminski@gmail.com>
2026-04-27 16:58:05 +0100

    Continue.

--- book/kilosort4_output/sorter_output/params.py
+++ book/kilosort4_output/sorter_output/params.py
@@ -3,4 +3,4 @@ offset = 0
 sample_rate = 30000.0
 dtype = 'int16'
 hp_filtered = False
-dat_path = ['C:/Users/Joe/work/git-repos/course-ephys-osss/book/kilosort4_output/sorter_output/recording.dat']
+dat_path = ['C:/Users/Jzimi/git-repos/course-ephys-osss/book/kilosort4_output/sorter_output/recording.dat']
