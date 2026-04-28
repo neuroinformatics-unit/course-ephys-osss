f2ce6efb32070425e994c1f01cd7a261b71a4f92
JoeZiminski <Joseph.j.ziminski@gmail.com>
2026-04-28 14:07:47 +0100

    First draft.

--- book/kilosort4_output/sorter_output/params.py
+++
@@ -1,6 +0,0 @@
-n_channels_dat = 384
-offset = 0
-sample_rate = 30000.0
-dtype = 'int16'
-hp_filtered = False
-dat_path = ['C:/Users/Joe/work/git-repos/course-ephys-osss/book/kilosort4_output/sorter_output/recording.dat']
